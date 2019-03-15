22da33db523c52313e6979db03a36af2edf6016f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-15 12:27:12 +0000

    Done.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,4 @@
 class StoreController < ApplicationController
-  skip_before_action :authenticate_user!, only:[:index]
   def index
     @categories = Category.all
     @items = Item.where("inventory > ?",0)
